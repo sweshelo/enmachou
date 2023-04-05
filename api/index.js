6399e3a1b592e58a4d412b10791da5caffec759c
sweshelo <sweshelo@gmail.com>
2023-04-05 14:41:51 +0900

    [feat] WebAPIを実装

--- api/index.js
+++ api/index.js
@@ -1,8 +1,7 @@
 const { JSDOM } = require('jsdom')
 const mysql = require('mysql');
 const fs = require('fs');
-const {resolve} = require('dns');
-const baseUrl = `https://p.eagate.573.jp/game/chase2jokers/ccj/ranking/index.html`
+const express = require("express");
 
 // 「yyyymmdd」形式の日付文字列に変換する関数
 function now() {
@@ -27,6 +26,13 @@ function now() {
   return yyyy + mm + dd + hh + mi + ss;
 };
 
+// 全角文字にする
+function toFullWidth(str) {
+  return str.replace(/[A-Za-z0-9]/g, function(s) {
+    return String.fromCharCode(s.charCodeAt(0) + 0xFEE0);
+  });
+}
+
 const connection = mysql.createConnection({
   host: 'mysql',
   user: 'ccj',
@@ -34,7 +40,8 @@ const connection = mysql.createConnection({
   database: 'ccj'
 });
 
-// Icon
+// const
+const baseUrl = `https://p.eagate.573.jp/game/chase2jokers/ccj/ranking/index.html`
 const regExp = /ranking_icon_([0-9]{1,2}).png/;
 
 const main = async() => {
@@ -47,15 +54,6 @@ const main = async() => {
         const dom = new JSDOM(r, 'text/html')
         const document = dom.window.document
         rankingData.push(...[...document.querySelector('#ranking_data').children].slice(1, 26).map(data => {
-          /*
-          return({
-            'rank': parseInt(data.querySelector('div').textContent),
-            'chara': data.querySelector('img').src,
-            'achievement': [...data.querySelectorAll('div')][1].querySelector('span').textContent,
-            'player': [...data.querySelectorAll('div')][1].querySelectorAll('p')[1].childNodes[1].textContent,
-            'point': parseInt([...data.querySelectorAll('div')][2].childNodes[0].textContent),
-          })
-          */
           const match = data.querySelector('img').src.match(regExp);
           const number = match ? match[1] : null;
           const username = [...data.querySelectorAll('div')][1].querySelectorAll('p')[1].childNodes[1].textContent
@@ -71,7 +69,6 @@ const main = async() => {
   })
 
   await Promise.all(fetchPromisses)
-  console.log('rankingData: ', rankingData)
 
   const create = []
   const sqlPromisses = rankingData.map((data) => {
@@ -92,32 +89,86 @@ const main = async() => {
           }
         }
         if(err){
-          console.log(err)
+          console.log(`[${now()}] ERROR @ SELECT - ${err}`)
           reject()
         }
       })
     })
   })
   await Promise.all(sqlPromisses)
-  console.log('promisses waited.')
+  console.log(`[${now()}] Promisses waited.`)
 
   // 新規ユーザを登録
   if(create.length > 0){
     const insertIntoUserQuery = "INSERT INTO users (user_name, ranking, achievement, chara, point) VALUES ?;";
     connection.query(insertIntoUserQuery, [create], (err, result) => {
-      console.log('create: ', err, result)
+      if(err){
+        console.error(`[${now()}] ERROR @ CREATE - ${err}`)
+      }else{
+        console.log(`[${now()}] created.`)
+      }
     })
   }
 
   const insertIntoTimelineQuery = "INSERT INTO timeline (user_name, ranking, achievement, chara, point) VALUES ?;";
   connection.query(insertIntoTimelineQuery, [rankingData], (err, result) => {
-    if(err) console.log(err)
-    if(result) console.log(result)
+    if(err){
+      console.error(`[${now()}] ERROR - @ UPDATE ${err}`)
+    }else{
+      console.log(`[${now()}] updated.`)
+    }
   })
 }
 
 main()
 setInterval(() => {
   main()
-  console.log('Recorded.' + now())
+  console.log(`[${now()}] recorded.`)
 }, 1000 * 60 * 5)
+
+// ==== Web API ==== //
+const app = express();
+const server = app.listen(4400, () =>  console.log("Node.js is listening to PORT:" + server.address().port));
+
+const ranking = (req, res) => {
+  const getLatestRankingFromTimelineQuery = "SELECT ranking, user_name, point, chara FROM (SELECT * FROM timeline ORDER BY created_at LIMIT 100) AS t ORDER BY ranking;";
+  connection.query(getLatestRankingFromTimelineQuery, (err, result) => {
+    if(result){
+      res.send(result)
+    }
+  })
+}
+
+const userinfo = (req, res) => {
+  const getUserTimelineFromTimelineQuery = "with records as (select timeline_id, user_name, point, row_number() over (partition by point order by timeline_id) as row_num from timeline) select * from timeline where timeline_id in (select timeline_id from records where row_num = 1) and user_name = ? order by created_at;"
+  connection.query(getUserTimelineFromTimelineQuery, [ toFullWidth(req.params.username) ], (err, result) => {
+    if(result){
+      // 増分を計算する
+      const pointDiff = result.map((record, i, arr) => i === 0 ? record.point : record.point - arr[i - 1].point).slice(1);
+      const average = pointDiff.reduce((acc, cur) => acc + cur, 0) / pointDiff.length;
+      const latestRecord = result[result.length - 1]
+      const response = {
+        'user': toFullWidth(req.params.username),
+        'achievement': latestRecord.achievement,
+        'chara': latestRecord.chara,
+        'online': (new Date() - new Date(latestRecord.created_at)) <= 20 * 60 * 1000,
+        'average': average,
+        'diff': pointDiff,
+        'log': result,
+      }
+      res.send(response)
+    }
+  })
+}
+
+const online = (req, res) => {
+  const getOnlineUserFromUsersQuery = "SELECT user_name FROM users WHERE updated_at > ?;"
+  const nMinutesAgoTime = (new Date(Date.now() - (req.params.threshold ? req.params.threshold : 20) * 1000 * 60))
+  connection.query(getOnlineUserFromUsersQuery, [ nMinutesAgoTime.toLocaleString('sv-SE', { timeZone: 'Asia/Tokyo' }) ], (err, result) => {
+    res.send(result)
+  })
+}
+
+app.get('/api/ranking', (req, res) => {ranking(req, res)})
+app.get('/api/users/:username', (req, res) => {userinfo(req, res)})
+app.get('/api/online/:threshold?', (req, res) => {online(req, res)})
