8dfcec976b17e79619f5dd5a8a133f7117250d95
sweshelo <sweshelo@gmail.com>
2023-06-09 15:24:52 +0900

    [fix] 名前を半角に

--- app/src/components/Player.jsx
+++ app/src/components/Player.jsx
@@ -1,3 +1,10 @@
+// 半角文字にする
+const toHalfWidth = (str) => {
+  return str.replace(/[Ａ-Ｚａ-ｚ０-９]/g, (s) => {
+    return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+  }).replace('．', '.');
+}
+
 export const Player = ({header, chara, name, isLink}) => {
   return(
     <div className="player">
@@ -5,7 +12,7 @@ export const Player = ({header, chara, name, isLink}) => {
         { chara && <img className="character" src={`https://p.eagate.573.jp/game/chase2jokers/ccj/images/ranking/icon/ranking_icon_${chara}.png`} /> }
         <div className="playerinfo-wrapper">
           <p>{header}</p>
-          <h2 className="playername ccj-font">{name}</h2>
+          <h2 className="playername ccj-font">{toHalfWidth(name)}</h2>
         </div>
       </a>
     </div>
