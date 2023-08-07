c3af897066cb37bff0d140e4bd8032375401ebbf
sweshelo <sweshelo@gmail.com>
2023-08-07 23:22:39 +0900

    [feat] ログイン後のトークンをlocalstorageで保持

--- app/src/redux/store.ts
+++ app/src/redux/store.ts
@@ -1,6 +1,7 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { composeWithDevTools } from 'redux-devtools-extension'
+import persistState from 'redux-localstorage'
 
 import rootReducer from './reducer.ts';
 import rootSaga from './saga.ts';
@@ -12,11 +13,11 @@ export default function configureStore() {
   const middlewares = [sagaMiddleware]
   const middlewareEnhancer = applyMiddleware(...middlewares)
 
-  const enhancers = [middlewareEnhancer]
+  const localstorageEnhancer = compose(persistState(['accountReducer'], { key: 'enmachou' }))
+  const enhancers = [middlewareEnhancer, localstorageEnhancer]
   const composedEnhancers = composeWithDevTools(...enhancers)
 
   const store = createStore(rootReducer, composedEnhancers)
-
   sagaMiddleware.run(rootSaga)
   return store
 }
